6cf1e43ef085b454304d98d0b8d427a15abb6796
Adam <adam@iii.com>
2019-02-13 01:44:54 +0800

    php type

---
+++ hello-php/basic/type.php
@@ -0,0 +1,20 @@
+<?php
+$n=555;
+echo $n."<br>";
+$n=55.12345;
+echo $n."<br>";
+$n=-432;
+echo $n."<br>";
+$n=017;
+echo $n."<br>";
+$n=0xFF;
+echo $n."<br>";
+$n=0xFFFF;
+echo $n."<br>";
+$n=null;
+echo $n."<br>";
+$n=undefinedornot.kkk;
+echo $n."<br>";
+unset($n);
+echo 'unset=>'.$n."<br>";
+?>
